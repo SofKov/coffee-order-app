0dbc41f2efae44bcc60842352a6f171f58527c79
Sofia K <sofiakovalevskaia@gmail.com>
2021-03-24 09:13:47 +0000

    updated files, created utils folder, created add, remove and list functions

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const yargs = require("yargs");
-const {addNote} = require("../utils/orders");
+const {addOrder, listOrders, removeOrder} = require("../utils/orders");
 
-console.log(process.argv);
-console.log(yargs.argv);
 
 const command = process.argv[2];
 
 if (command == "add"){
     console.log("Adding new coffee order:");
-    addNote(yargs.argv.order);
+    console.log(yargs.argv.order);
+    addOrder(yargs.argv.order);
 
-    
 } else if (command == "remove"){
     console.log("Removing order from list:");
+    console.log(yargs.argv.order);
+    removeOrder(yargs.argv.order);
+
 } else if (command == "list"){
-    console.log("Displaying orders in list:")
+    console.log("Displaying all orders below:");
+    listOrders();
+
 } else {
     console.log("Invalid input");
 };
-
--- utils/orders.js
+++ utils/orders.js
@@ -10,20 +10,39 @@ const loadOrders = () => {
     }
 };
 
-const saveOrders = (allOrder) => {
+const saveOrders = (allOrders) => {
     const ordersJson = JSON.stringify(allOrders);
     fs.writeFileSync("src/orders.json", ordersJson);
 };
 
 const addOrder = (myOrder) => {
     const allOrders = loadOrders ();
-    allOrders.push({order: myOrder});
+    allOrders.push({orderAdded: myOrder});
 
     saveOrders(allOrders);
+};
+
+const listOrders = () => {
+    const allOrders = loadOrders();
+    allOrders.map(order => {
+        console.log(order);
+    });
+};
+
+const removeOrder = (orderToRemove) => {
+    const allOrders = loadOrders();
+
+    const ordersToKeep = allOrders.filter( order =>{
+        return order.orderAdded != orderToRemove;
+    });
+
+    saveOrders(ordersToKeep);
 };
 
 
 
 module.exports = {
     addOrder,
+    listOrders,
+    removeOrder
 };
